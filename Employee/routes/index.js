db0e8e82a83f99076dd6a29f21ab86c17507501e
boribap <bsww201@naver.com>
2020-01-16 10:44:17 +0900

    employee - 필요없는 라우터 제거

--- Employee/routes/index.js
+++ Employee/routes/index.js
@@ -21,22 +21,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/empSignup', function(req, res){
+router.get('/empSignup', function(req, res) {
   res.render('empSignup');
-})
+});
 
-router.get('/empLogin', function(req, res){
+router.get('/empLogin', function(req, res) {
   res.render('empLogin');
-})
+});
 
-router.get('/main', function(req, res){
-  res.render('main');
-})
-
-router.get('/empQrcode', function(req, res){
+router.get('/empQrcode', function(req, res) {
   res.render('empQrcode');
-})
-
+});
 
 router.post('/empSignup', function(req, res) {
   console.log(req.body);
@@ -79,84 +74,5 @@ router.post('/empLogin', function(req, res) {
     }
   });
 });
-
-
-router.post('/balance', auth, function(req, res){ // 계좌 잔액 조회
-  var userData = req.decoded;
-  var finusenum = req.body.fin_use_num;
-
-  var sql ="SELECT * FROM user WHERE id = ?"
-  connection.query(sql, [userData.userId], function(err, result){
-      if(err){
-          console.error(err);
-          throw err;
-      }
-      else {
-          console.log(result);
-          var random = Math.floor(Math.random() * 1000000000) + 1;    
-          var ranId = "T991605830U" + random;
-          var option = {
-              method: 'GET',
-              url: 'https://testapi.openbanking.or.kr/v2.0/account/balance/fin_num',
-              headers: {
-                'Authorization': 'Bearer ' + result[0].accesstoken
-               },
-              qs : {
-                  
-                  bank_tran_id : ranId,
-                  fintech_use_num : finusenum,
-                  tran_dtime : '20200109145559'
-              }
-            }
-            request(option, function(error,response, body){
-              console.log(body);
-              var parseData = JSON.parse(body);
-              res.json(parseData);
-          })  
-      }
-})
-})
-
-
-router.post('/balancelist', auth, function(req, res){   //거래내역조회 
-  var userData = req.decoded;
-  var finusenum = req.body.fin_use_num;
-
-  var sql ="SELECT * FROM user WHERE id = ?"
-  connection.query(sql, [userData.userId], function(err, result){
-      if(err){
-          console.error(err);
-          throw err;
-      }
-      else {
-          console.log(result);
-          var random = Math.floor(Math.random() * 1000000000) + 1;    
-          var ranId = "T991605830U" + random;
-          var options = {
-              method: 'GET',
-              url: 'https://testapi.openbanking.or.kr/v2.0/account/transaction_list/fin_num',
-              headers: {
-                Authorization: 'Bearer ' + result[0].accesstoken
-              },
-              qs : {
-                  bank_tran_id : ranId,
-                  fintech_use_num : finusenum,
-                  inquiry_type :'A',
-                  inquiry_base:'D',
-                  from_date : '20190101',
-                  to_date : '20190110',
-                  sort_order :'D',
-                  tran_dtime : '20200110102959'
-              }
-            }
-            request(options, function (error, response, body) { 
-              console.log(body);
-              var parseData = JSON.parse(body);
-              res.json(parseData);
-            })
-            
-      }
-})
-})
 
 module.exports = router;
