ad5d11247b43d25120d20dbe7db6a60349286a39
eungalchi <tbvjzlem3@gmail.com>
2020-01-16 20:43:01 +0900

    버튼 예쁘게

--- Franchise/app.js
+++ Franchise/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,6 +22,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors())
 
 app.use('/', indexRouter); // 위에 정의되어 있음
 app.use('/users', usersRouter);
