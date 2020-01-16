06895dfe71eb503c2655d117e976b04990e2766b
boribap <bsww201@naver.com>
2020-01-16 19:22:33 +0900

    수정

--- Enterprise/routes/transaction.js
+++ Enterprise/routes/transaction.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 var auth = require('../lib/auth');
 var jwt = require('jsonwebtoken');
-var request =require('request'); //다 넣어줘야함
+var request = require('request'); //다 넣어줘야함
 
-var tokenKey = "fintechAcademy0$1#0@6!";
+var tokenKey = 'fintechAcademy0$1#0@6!';
 
 var mysql = require('mysql');
 var config = require('../../config/config.json');
@@ -19,28 +19,28 @@ var connection = mysql.createConnection({
 connection.connect;
 
 //get 필요
-router.get('/enterTransaction', function(req, res){
+router.get('/enterTransaction', function(req, res) {
   res.render('enterTransaction');
-})
+});
 
-
-router.post('/enterTransaction', auth, function(req, res){   //회사 거래내역조회 
+router.post('/enterTransaction', auth, function(req, res) {
+  //회사 거래내역조회
   var userData = req.decoded;
   var finusenum = req.body.fin_use_num;
 
-  console.log(userData)
+  console.log(userData);
 
-  var sql = "SELECT distinct employee.name emp_name,franchise.name fran_name,transaction.enterpriseCode, transaction.menu, transaction.price FROM transaction,franchise,employee WHERE transaction.enterpriseCode = 67 and transaction.franID=franchise.franId and transaction.employID=employee.ID and transaction.isPay=0"
-  connection.query(sql, [userData.userCode], function(err, result){
-      if(err){
-          console.error(err);
-          throw err;
-      }else {
-        console.log(result);
-        res.json(result);
-            
-      }
-})
-})
+  var sql =
+    'SELECT distinct employee.name emp_name,franchise.name fran_name,transaction.enterpriseCode, transaction.menu, transaction.price FROM transaction,franchise,employee WHERE transaction.enterpriseCode = ? and transaction.franID=franchise.franId and transaction.employID=employee.ID and transaction.isPay=0';
+  connection.query(sql, [userData.userCode], function(err, result) {
+    if (err) {
+      console.error(err);
+      throw err;
+    } else {
+      console.log(result);
+      res.json(result);
+    }
+  });
+});
 
 module.exports = router;
